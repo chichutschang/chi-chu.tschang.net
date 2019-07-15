40801d93ddf7932fe9b9add9a991db12c4d4b977
Chi-Chu Tschang <chichutschang@users.noreply.github.com>
2019-07-14 22:31:42 -0700

    added progress bar to reading.ejs

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ var logger = require('morgan');
 var ejsLayouts = require('express-ejs-layouts')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var corsOptions = {
+//   origin: 'http://127.0.0.1:3000',
+//   optionsSuccessStatus: 200
+// }
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors({
-  origin: 'http://127.0.0.1:3000'
-}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -41,5 +42,19 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+//enable all cors requests
+app.use(cors({
+  origin: 'http://127.0.0.1:3000'
+}));
+
+// app.get('/products/:id', cors(corsOptions), function(req, res, next){
+//     res.json({msg: 'This is a CORS-enabled for all origins!'})
+// })
+
+app.listen(80, function(){
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 module.exports = app;
