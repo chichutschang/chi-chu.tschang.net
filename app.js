ebaea8079f2c17aa7a203ff32c19e73923faca83
chichutschang <chichutschang@gmail.com>
2019-07-26 20:30:40 -0700

    Uninstall express-http-to-https npm package

--- app.js
+++ app.js
@@ -40,26 +40,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//redirect http to https
-var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
-
-// Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
-app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/insecure', function (req, res) {
-  res.send('Dangerous!');
-});
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
-//
-// app.listen(8080, function () {
-//   console.log('Example app listening on port 8080 insecurely!');
-// });
 
 module.exports = app;
