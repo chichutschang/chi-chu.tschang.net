45c8c0ea401121d0b12559ab17002907a980be38
chichutschang <chichutschang@gmail.com>
2019-07-27 22:24:04 -0700

    Changed var to const on line 12 of app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aws = require('aws-sdk');
 var app = express();
-var goodreads = require('goodread-api-node');
+const goodreads = require('goodread-api-node');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
