9b580d7a393cafe2e905acf90397ede60509ea95
chichutschang <chichutschang@gmail.com>
2020-05-24 19:54:13 -0700

    Changed app.listen in app.js

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //set up listen port
-app.listen(process.env.PORT || 3000)
+app.listen((app.get('port'), function (){
+  console.log('Server has started....')
+}))
+//app.listen(process.env.PORT || 3000)
 
 //set up folders to use
 app.use(logger('dev'));
