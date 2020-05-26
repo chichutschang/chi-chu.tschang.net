e11a0e060f2fb36372c33f5a578601a3807709b1
chichutschang <chichutschang@gmail.com>
2020-05-25 21:25:42 -0700

    Added var readingRouter to index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 var express = require('express');
 var router = express.Router();
+var readingRouter = require('./reading');
 var async = require('async');
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
