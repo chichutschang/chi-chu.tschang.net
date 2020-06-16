3bae3c8e451c17699e0d07e5bd9ad11998d4b6e1
chichutschang <chichutschang@gmail.com>
2020-06-15 22:35:32 -0700

    Deleted extra line

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ var client = require('../db');
 let currentlyreading = require('../models/currentlyreading');
 let reading = require('../models/read');
 
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
