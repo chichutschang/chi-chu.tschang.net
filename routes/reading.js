53b28e42fb068e3528a5b72e1ed7cb81514c5753
chichutschang <chichutschang@gmail.com>
2020-06-20 17:41:05 -0700

    Changed userID=process.env.userID

--- routes/reading.js
+++ routes/reading.js
@@ -8,9 +8,9 @@ var router = express.Router();
 const parseString = require('xml2js').parseString;
 var client = require('../db');
 //key, URL and UserID to access Goodreads API
-const key = process.env.GOODREADS_KEY
-const database = process.env.DATABASE_URL || 8080;
-const userID='1589736';
+const key = process.env.GOODREADS_KEY;
+const database = process.env.DATABASE_URL;
+const userID=process.env.userID;
 //Goodreads XML URL
 const currentlyreadURL = 'https://www.goodreads.com/review/list/'+userID+'.xml?key='+key+'&v=2&shelf=currently-reading'
 const readURL1 = 'https://www.goodreads.com/review/list/'+userID+'.xml?key='+key+'&v=2&shelf=read&per_page=200&page=1'
