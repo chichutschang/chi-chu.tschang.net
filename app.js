a02cc12325eb86c32dcaa36a7586bff27c8cab60
chichutschang <chichutschang@gmail.com>
2020-06-20 17:39:47 -0700

    Added (process.env.PORT || 3000) to app.listen()

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var readingRouter = require('./routes/reading');
 var client = require('./db');
-const database = process.env.DATABASE_URL
+const database = process.env.DATABASE_URL;
 const request = require('request');
 
 // set up view engine for views folder
@@ -17,19 +17,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //set up listen port
-app.listen((app.get('port'), function (){
-  console.log('Server has started....')
-}))
+// app.listen((app.get('port'), function (){
+//   console.log('Server has started....')
+// }))
 //app.listen(process.env.PORT || 3000)
 
 //set up MongoDB
+//console.log(database)
 client.connect(database, (err) => {
   if (err) {
     console.log('Unable to connect to MongoDB...')
+    console.err(err);
     process.exit(1);
   } else {
-    app.listen(3000, () => {
-      console.log('Listening on Port 3000 from app.js...');
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listening on Port ${process.env.PORT} from app.js...`);
+      //console.log('Listening on Port 3000 from app.js...');
     })
   }
 });
@@ -43,6 +46,7 @@ app.use(express.static(__dirname +'/static', {dotfiles: 'allow'}));
 //set up routes/reading.js to check Goodreads for currently reading and read books
 app.use((req, res, next) => {
   req.on = readingRouter
+  console.dir(readingRouter);
   next();
 });
 //set up routes/index.js for all pages
