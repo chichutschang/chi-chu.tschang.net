88db8add499767400e00f2c9b5a636ff3f983643
chichutschang <chichutschang@gmail.com>
2020-06-23 17:53:25 -0700

    Deleted code listening to same port as server

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var ejsLayouts = require('express-ejs-layouts')
 var app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var readingRouter = require('./routes/reading');
+var readingRouter = require('./routes/currentlyreading');
 var client = require('./db');
 const database = process.env.DATABASE_URL;
 const request = require('request');
@@ -16,14 +16,7 @@ const request = require('request');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//set up listen port
-// app.listen((app.get('port'), function (){
-//   console.log('Server has started....')
-// }))
-//app.listen(process.env.PORT || 3000)
-
 //set up MongoDB
-//console.log(database)
 client.connect(database, (err) => {
   if (err) {
     console.log('Unable to connect to MongoDB...')
@@ -31,13 +24,8 @@ client.connect(database, (err) => {
     process.exit(1);
   } else {
       console.log('Connected to MongoDB from app.js...')
-      //app.listen(process.env.PORT || 3000, () => {
-      //console.log(`Listening on Port ${process.env.PORT} from app.js...`);
-      //console.log('Listening on Port 3000 from app.js...');
-    //})
   }
 });
-
 
 //set up folders to use
 app.use(logger('dev'));
