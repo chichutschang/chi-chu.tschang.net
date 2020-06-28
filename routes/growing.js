12f937fbdfe6cd7f3bd04ff16e5dd4e79cf2ab26
chichutschang <chichutschang@gmail.com>
2020-06-28 13:06:55 -0700

    Added routes/growing.js

--- routes/growing.js
+++ routes/growing.js
@@ -6,14 +6,29 @@ const uri = "mongodb+srv://tschang:82H!ghland@plants-l7uuf.mongodb.net/plants?re
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //var data;
-function grow(){
+function grow(callback){
   client.connect(err => {
-    const collection = client.db("tomato").collection("february2019");
-    // perform actions on the collection object
-    //console.dir(collection);
-    return collection
+    let collection = client.db("tomato").collection("february2019");
+      // perform actions on the collection object
+      //console.dir(collection['s'])
+      collection.find({topic: 'sensor/moisture'}).toArray((err, results) => {
+      //return data
+        for (var i = 0; i < results.length; i++){
+          
+          //console.log(results)
+          //console.log(results[i].value)
+          //console.log(results[i].time)
+          }  
+      })
+      // .then(results => {
+      //   return results
+      // })
+      // .catch(error => console.error(error))
+    });
+
+    //return collection
     //client.close();    
-  });  
+  //});  
 }
 
 grow();
