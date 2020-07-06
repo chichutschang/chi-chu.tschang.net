d1c6627609f3d6f317e5b1bbdde12a8a07470f70
Chi-Chu Tschang <chichutschang@users.noreply.github.com>
2020-07-05 18:19:22 -0700

    Added .sort('time')

--- routes/growing.js
+++ routes/growing.js
@@ -6,20 +6,30 @@ const uri = "mongodb+srv://tschang:82H!ghland@plants-l7uuf.mongodb.net/plants?re
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //var data;
-function grow(callback){
+var moisture = function(){
   client.connect(err => {
     let collection = client.db("tomato").collection("february2019");
       // perform actions on the collection object
       //console.dir(collection['s'])
-      collection.find({topic: 'sensor/moisture'}).toArray((err, results) => {
+      collection.find({topic: 'sensor/moisture'}).sort('time').toArray((err, results) => {
       //return data
-        for (var i = 0; i < results.length; i++){
+        if(err){
+          console.error(err);
+          res.statusCode = 500;
+          res.send({
+            result: 'error',
+            err: err.code
+          })
+        } 
+          res.send(results)
+      
+      //for (var i = 0; i < results.length; i++){
           
           //console.log(results)
           //console.log(results[i].value)
           //console.log(results[i].time)
-          }  
-      })
+          //}  
+      //})
       // .then(results => {
       //   return results
       // })
@@ -29,8 +39,10 @@ function grow(callback){
     //return collection
     //client.close();    
   //});  
+  })
 }
+//grow();
 
-grow();
-
-module.exports = router;
+module.exports = {
+  moisture: moisture
+}
